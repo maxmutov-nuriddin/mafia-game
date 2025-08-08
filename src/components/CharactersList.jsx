8279ba61495d9c6fa58a2a79efcf4cede9108cd4
Maxmutov Nuriddin <mahmudovnuriddin35@gmail.com>
2025-08-08 16:36:18 +0500

    update CharactersList.jsx

--- src/components/CharactersList.jsx
+++ src/components/CharactersList.jsx
@@ -13,7 +13,7 @@ const CharacterList = ({ character }) => {
           className="w-20 h-20 object-cover mx-auto"
         />
         <h3 className="text-2xl font-black mt-1">{character.name}</h3>
-        <p className="text-md ">{character.description}</p>
+        <p className="text-md font-bold"><i>{character.description}</i></p>
       </div>
     </div>
   );
